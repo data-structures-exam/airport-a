6443ff0c10d019e754f472d65f560efc49f302b8
JoelnerD4521 <122645810+JoelnerD4521@users.noreply.github.com>
2023-01-13 23:06:41 +0100

    Add files via upload

---
+++ Bagagem.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Bagagem.h"
+#define Tam 50
+
+typedef struct tbagagem{
+char nome_passageiro[Tam];
+ int num_voo;
+ int peso_kg;
+		
+}Bagagem;
+
+Bagagem *criar_bagagem(char dados)
+{
+	
+	
+}
---
+++ Bagagem.h
@@ -0,0 +1,11 @@
+
+typedef struct tbagagem Bagagem;
+Bagagem *criar_bagagem(char dados);
+
+
+
+
+
+
+
+
