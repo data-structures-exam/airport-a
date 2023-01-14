3e427502f32f9a7e4409807bc6f63ec2186b8724
Samuel Pedro <redsamuel.cruz7@gmail.com>
2023-01-14 21:34:28 +0100

    add essentials to bagagem adt

--- bagagem.c
+++ bagagem.c
@@ -6,12 +6,30 @@
 struct tBagagem{
 	char nome_passageiro[STR_TAM_MAX];
 	int num_voo;
-	int peso_kg;
-		
+	float peso_kg;
 };
 
-Bagagem *criar_bagagem(char dados)
-{
-	
-	
+Bagagem *criar_bagagem(char nome_passageiro[], int num_voo, float peso_kg) {
+	Bagagem *bag = (Bagagem *)malloc(sizeof(Bagagem));
+	if (!bag) {
+		printf ("Erro: malloc falhou em 'criar_bagagem'\n");
+		return NULL;
+	}
+
+	strcpy(bag->nome_passageiro, nome_passageiro);
+	bag->num_voo = num_voo;
+	if (peso_valido(peso_kg))
+		bag->peso_kg = peso_kg;
+	else
+		bag->peso_kg = 0.0;
+
+	return bag;
+}
+
+int peso_valido(float peso) {
+	return (peso > 0.0 && peso <= 32.0); // (0; 32]
+}
+
+void destruir_bagagem(Bagagem *bag) {
+	free(bag);
 }
--- bagagem.h
+++ bagagem.h
@@ -2,7 +2,9 @@
 #define BAGAGEM_H
 
 typedef struct tBagagem Bagagem;
-Bagagem *criar_bagagem(char dados);
+Bagagem *criar_bagagem(char nome_passageiro[], int num_voo, float peso_kg);
+int peso_valido(float peso);
+void destruir_bagagem(Bagagem *bag);
 
 #endif
 
