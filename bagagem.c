35173c6ef2b086084f7d1e296c9ee9210d4eb8ee
Samuel Pedro <redsamuel.cruz7@gmail.com>
2023-01-14 21:19:46 +0100

    fixed typo

--- bagagem.c
+++ bagagem.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Bagagem.h"
+#include "bagagem.h"
 #define Tam 50
 
-typedef struct tbagagem{
+typedef struct tBagagem{
 char nome_passageiro[Tam];
  int num_voo;
  int peso_kg;
