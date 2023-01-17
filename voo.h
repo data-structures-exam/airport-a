e45be97789c69527a3cfa5b8c88f40ccad52bc18
Samuel Pedro <redsamuel.cruz7@gmail.com>
2023-01-17 14:35:38 +0100

    add missing functions and parameters

--- voo.h
+++ voo.h
@@ -1,10 +1,15 @@
 #ifndef VOO_H
 #define VOO_H
+
 #include "data.h"
+#include <stdbool.h>
 
 typedef struct tVoo Voo;
 
 Voo *criar_voo(int num, Data data, char origem[], char destino[]);
+void carregar_palete(Voo *voo, Palete *p);
+bool porao_cheio(Voo *voo);
 void destruir_voo(Voo *voo);
+void fechar_voo(Voo *voo);
 
 #endif
