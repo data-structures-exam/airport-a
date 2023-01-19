042cbc3be6ba58ce9212132c3a7f29c6844b6a3d
Samuel Pedro <redsamuel.cruz7@gmail.com>
2023-01-19 21:58:26 +0100

    add missing functions

--- palete.h
+++ palete.h
@@ -2,11 +2,15 @@
 #define PALETE_H
 
 #include "pilha_bagagem.h"
+#include <stdbool.h>
 
 typedef struct tPalete Palete;
 
 Palete *criar_palete(int num_voo);
 void inserir_bagagens(Palete *p, Pilha_Bagagem *pilha_bagagens);
+bool palete_cheia(Palete *p);
+bool limite_pilhas_atingido(Palete *p);
+bool palete_vazia(Palete *p);
 void destruir_palete(Palete *p);
 
 #endif
