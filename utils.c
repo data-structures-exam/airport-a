60bd860a2d165504539792cdd64d90cbe823e408
Samuel Pedro <redsamuel.cruz7@gmail.com>
2023-01-25 22:10:21 +0100

    address buffer issue

--- utils.c
+++ utils.c
@@ -5,6 +5,7 @@ int ler_linha(char str[], int num_char) {
 	while ((ch = getchar()) != '\n')
 		if (i < n)
 			str[i++] = ch;
+	getchar(); // remover o '\n' do buffer
 	str[i] = '\0';
 	return i; // número de caracteres lido
 }
