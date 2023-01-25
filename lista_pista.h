5e1092941c4dc850af8d3d29bade8f3ac5560387
Samuel Pedro <redsamuel.cruz7@gmail.com>
2023-01-25 18:40:21 +0100

    remove unnecessary lista_pista adt

--- lista_pista.h
+++
@@ -1,8 +0,0 @@
-
-typedef struct tListaPista ListaPista;
-
-void adicionar_voo_pistaDecolagem(ListaPista *lPista, int numero_pista, Voo *voo);
-void retirar_voo_pistaDecolagem(ListaPista *lPista, int numero_pista, Voo *voo);
-void adicionar_voo_pistaAterragem(ListaPista *lPista, int numero_pista, Voo *voo);
-void retirar_voo_pistaAterragem(ListaPista *lPista, int numero_pista, Voo *voo);
-void destruir_lista_pista(ListaPista *lPista);
