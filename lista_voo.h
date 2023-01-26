07b74b0dadf731e5aff01513c228c1085441095e
Samuel Pedro <redsamuel.cruz7@gmail.com>
2023-01-26 11:13:59 +0100

    lista_voo.c

--- lista_voo.h
+++ lista_voo.h
@@ -13,6 +13,8 @@ Lista_Voo *criar_lista_voo();
 void remover_voo(Lista_Voo **L, int num_voo);
 int verificar_voo(Lista_Voo *L, int num_voo);
 void imprimir_voos(Lista_Voo *L);
+Lista_Voo *buscar_voo(Lista_Voo *L, int i);
+int count_voos(Lista_Voo *L);
 int lista_vazia(Lista_Voo *L);
 void destruir_lista_voo(Lista_Voo *L);
 void adicionar_voo_lista(Lista_Voo **L, Voo *v);
