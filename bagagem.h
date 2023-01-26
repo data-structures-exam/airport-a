9f9530486b53780511e689b7260936547ed3c8b9
JoelnerD4521 <122645810+JoelnerD4521@users.noreply.github.com>
2023-01-26 23:33:53 +0100

    Update bagagem.h

--- bagagem.h
+++ bagagem.h
@@ -16,4 +16,5 @@ typedef struct tBagagem Bagagem;
 Bagagem *criar_bagagem(char nome_passageiro[], int num_voo, float peso_kg);
 int peso_valido(float peso);
 void destruir_bagagem(Bagagem *bag);
+
 #endif
