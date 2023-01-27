e5e691cecdf4ad3117b43537656ef006fea580ac
Samuel Pedro <redsamuel.cruz7@gmail.com>
2023-01-27 19:32:43 +0100

    remove buffer fix attempt and fix by one bug

--- utils.c
+++ utils.c
@@ -2,10 +2,10 @@
 
 int ler_linha(char str[], int num_char) {
 	int ch, i = 0;
-	while ((ch = getchar()) != '\n')
-		if (i < num_char)
+	while ((ch = getchar()) != '\n') {
+		if (i < num_char - 1)
 			str[i++] = ch;
-	getchar(); // remover o '\n' do buffer
+	}
 	str[i] = '\0';
 	return i; // número de caracteres lido
 }
